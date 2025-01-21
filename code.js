9209cb74f2ab7d5729c6d290906d9ae5ef7fdd2d
FonnFon <143230785+FonnFon@users.noreply.github.com>
2025-01-21 19:16:32 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,33 @@
+var wizard, wizardWalk;
+var fundoImg;
+var wizardVelocityX;
+
+
+function preload() {
+
+    fundoImg = loadImage("fundo.jpg");
+    /*wizardWalk = loadAnimation("wizard_sprite_1.png",
+    "wizard_sprite_2.png")*/
+}
+
+function setup() {
+    createCanvas(1600, 682);
+    /*wizard = createSprite(50, 50);
+    wizard.addAnimation("walking", wizardWalk)  
+    wizard.velocityX = 5;
+    wizard.velocityY = 5*/
+}
+
+function draw() {
+    background(fundoImg);
+
+
+    /*if(keyDown("d")) {
+        wizard.velocityX = velocityX + 5
+    }
+
+    if(keyDown("a")) {
+        wizard.velocityY = velocityX - 5
+    }*/
+    drawSprites();
+}
